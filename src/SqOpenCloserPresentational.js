653939550eb8ed530925d6f42e6be0fe134fc364
Anwesh43 <anweshthecool0@gmail.com>
2020-05-16 21:50:37 +0530

    adding SqOpenCloserPresentational component

---
+++ src/SqOpenCloserPresentational.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {useStyle} from './hooks'
+const Square = ({i, w, h, scale}) => {
+    const {getBlockOpenStyle} = useStyle(w, h, scale)
+    return <div style = {getBlockOpenStyle(i)}>
+    </div>
+}
+
+const SqOpenCloserPresentational = ({w, h, scale, onClick}) => {
+    return <div onClick = {onClick}>
+        {[0, 1].map(i => <Square i = {i} w = {w} h = {h} scale = {scale}/>)}
+    </div>
+}
+
+export default SqOpenCloserPresentational
